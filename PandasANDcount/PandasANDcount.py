579b6f934a44b21c39c1ca02d46cfb3077c9c026
pratapvardhan <pratapgr8@gmail.com>
2014-02-02 18:58:52 +0530

    Added result plot

--- PandasANDcount/PandasANDcount.py
+++ PandasANDcount/PandasANDcount.py
@@ -13,20 +13,21 @@ t_xlen = range(1,2*10**4,10**2)
 for xlen in t_xlen:
     tb = pd.DataFrame(np.random.randint(0,5,size=(xlen,2)),columns=['w1','w2'])
     t0=time.time()
-    v1 = len(tb[(tb['w1']>0) & (tb['w2']>0)]) # type1
+    v1 = len(tb[(tb['w1']>0) & (tb['w2']>0)]) # Type1
     t1=time.time()
-    v2 = int(sum((tb['w1']>0) & (tb['w2']>0))) # type2
-    #v2 = (sum((tb['w1']>0) & (tb['w2']>0))) # same as type2
-    #v2 = int(((tb['w1']>0) & (tb['w2']>0)).sum()) # as slow as type1
+    v2 = int(sum((tb['w1']>0) & (tb['w2']>0))) # Type2
+    #v2 = (sum((tb['w1']>0) & (tb['w2']>0))) # same as Type2
+    #v2 = int(((tb['w1']>0) & (tb['w2']>0)).sum()) # as slow as Type1
     t2=time.time()
     x1.append(t1-t0)
     x2.append(t2-t1)
 
 import matplotlib.pyplot as plt
-plt.plot(t_xlen, x1,'b',label='1nd Method')
-plt.plot(t_xlen, x2,'r',label='2nd Method')
+plt.plot(t_xlen, x1,'b',label='Type-1 Method')
+plt.plot(t_xlen, x2,'r',label='Type-2 Method')
 plt.xlabel('Length of DataFrame')
 plt.ylabel('Time (in sec)')
 plt.title('Pandas DataFrame - Count on AND operation')
 plt.legend(loc='upper left')
-plt.show()
+#plt.show()
+plt.savefig('PandasANDCount.png')
